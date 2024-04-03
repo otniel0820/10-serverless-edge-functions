10390bfd8a300eebaba4885cb1ed4cee4f1c30c7
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-04-03 22:36:18 +0200

    github to netlify to discord

--- netlify/functions/github-discord/github-discord.ts
+++ netlify/functions/github-discord/github-discord.ts
@@ -1,43 +1,90 @@
-import type { Context } from "@netlify/functions"
+import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
+const notify = async ( message: string ) => {
 
-const notify = async(message:string)=>{
-    
-        const body = {
-            content: message,
-            embeds: [
-                {
-                    image: {url: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3IxY2tpazF6d3M2c2hsdWI1aWk3NXR3cDBmNTN0dzAwcXoxaTJ0cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/du3J3cXyzhj75IOgvA/giphy.gif'}
-                }
-            ]
-        }
+  const body = { content: message };
 
-        const resp = await fetch(process.env.DISCORD_WEBHOOK_URL ?? '',{
-            method: 'POST', 
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        })
+  const resp = await fetch( process.env.DISCORD_WEBHOOK_URL ?? '', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify( body ),
+  } );
 
-        if (!resp.ok) {
-            console.log("Error sending message to discord");
-            return false
-            
-        }
+  if ( !resp.ok ) {
+    console.log( 'Error sending message to discord' );
+    return false;
+  }
+  return true;
+};
 
-        return true
-    
+const onStar = ( payload: any ): string => {
+
+  const { action, sender, repository } = payload;
+
+  return `User ${ sender.login } ${ action } star on ${ repository.full_name }`;
+
 }
-export default async (req: Request, context: Context) => {
-  const myImportantVariable = process.env.MY_IMPORTANT_VARIABLE;
 
-  await notify('hola mundo desde netlify dev')
-  if (!process.env.MY_IMPORTANT_VARIABLE) {
-    return new Response("Error: La variable de entorno MY_IMPORTANT_VARIABLE no está definida", { status: 500 });
+const onIssue = ( payload: any ): string => {
+
+  const { action, issue } = payload;
+
+  if ( action === 'opened' ) {
+    return `An issue was opened with this title ${ issue.title }`;
   }
 
-  console.log('Hola mundo desde los logs');
-  
-  return new Response("Valor de MY_IMPORTANT_VARIABLE: " + myImportantVariable);
+  if ( action === 'closed' ) {
+    return `An issue was closed by ${ issue.user.login }`;
+  }
+
+  if ( action === 'reopened' ) {
+    return `An issue was reopened by ${ issue.user.login }`;
+  }
+
+
+  return `Unhandled action for the issue event ${ action }`;
+
+
 }
+
+
+
+const handler: Handler = async ( event: HandlerEvent, context: HandlerContext ) => {
+
+  const githubEvent = event.headers[ 'x-github-event'] ?? 'unknown';
+  const payload = JSON.parse( event.body ?? '{}' );
+  let message: string;
+
+  console.log(payload);
+
+  switch( githubEvent ) {
+
+    case 'star':
+      message = onStar( payload );
+    break;
+
+    case 'issues':
+      message = onIssue(payload);
+    break;
+
+
+    default:
+      message = `Unknown event ${ githubEvent }`;
+
+  }
+
+
+  await notify( message );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify( {
+      message: 'done',
+    } ),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+};
+
+export { handler };
