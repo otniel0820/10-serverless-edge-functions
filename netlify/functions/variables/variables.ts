68df52689217d1799ab6e307937cd13b857f0ef5
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-04-03 14:41:17 +0200

    config edge functions with netlify

---
+++ netlify/functions/variables/variables.ts
@@ -0,0 +1,10 @@
+import type { Context } from "@netlify/functions"
+
+export default async (req: Request, context: Context) => {
+  const myImportantVariable = process.env.MY_IMPORTANT_VARIABLE;
+  if (!process.env.MY_IMPORTANT_VARIABLE) {
+    return new Response("Error: La variable de entorno MY_IMPORTANT_VARIABLE no está definida", { status: 500 });
+  }
+
+  return new Response("Valor de MY_IMPORTANT_VARIABLE: " + myImportantVariable);
+}
